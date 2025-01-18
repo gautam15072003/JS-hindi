b2a627d1de2da2424712609be0ef08ce5f7295ba
gautam15072003 <gautamsaini1507@gmail.com>
2025-01-18 15:36:26 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+/*let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+let valueInNumber = Number(score)   
+console.log(typeof valueInNumber )
+console.log(valueInNumber) //NaN
+*/
+
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+/* 1- true,0-false 
+    "" -  false
+    "gautam"-  true
+    */
