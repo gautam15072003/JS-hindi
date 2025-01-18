1f4f68bf644849b67faa6262439a35996842fc29
gautam15072003 <gautamsaini1507@gmail.com>
2025-01-18 18:18:10 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/*let score = "33abc"
 
 console.log(typeof score)
 console.log(typeof (score))
@@ -15,7 +15,45 @@ console.log(booleanIsLoggedIn)
     "" -  false
     "gautam"-  true
     */
-   let someNumber = 33
+/*  let someNumber = 33
    let stringSomeNumber = String(someNumber)
    console.log(stringSomeNumber)
-   console.log(typeof stringSomeNumber )
+   console.log(typeof stringSomeNumber )    */
+
+
+// -----------------------------operations--------------------------------------
+
+let value = 3
+let negativeValue = -value
+// console.log(negativeValue);
+
+// +,-,*,**->raies to the power,/,%->remainder
+
+// let str1 = "hello"
+// let str2 ="Gautam"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log( "1" + 2);
+// console.log( 1 + "2");
+// console.log( "1" + "2");
+// console.log( "1" + 2 + 2);
+// console.log( 1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3= 2+2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter)
+// postfix operator
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// // x2 is 4n; y2 is 3n
