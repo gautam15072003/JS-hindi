ebb0d65993f26ae67863597f1ad9668105c04097
gautam15072003 <gautamsaini1507@gmail.com>
2025-01-18 15:48:14 +0000

    conversions of datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
-/*let score = "33abc"
+let score = "33abc"
 
 console.log(typeof score)
 console.log(typeof (score))
 let valueInNumber = Number(score)   
 console.log(typeof valueInNumber )
 console.log(valueInNumber) //NaN
-*/
+
 
 let isLoggedIn = 1 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
@@ -15,3 +15,7 @@ console.log(booleanIsLoggedIn)
     "" -  false
     "gautam"-  true
     */
+   let someNumber = 33
+   let stringSomeNumber = String(someNumber)
+   console.log(stringSomeNumber)
+   console.log(typeof stringSomeNumber )
