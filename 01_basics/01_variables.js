1cc518d0310ea2619c95ea88abf348f03e879f51
gautam15072003 <gautamsaini1507@gmail.com>
2025-01-18 14:17:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1223445
+let accountEmail = "gautam.google.com"
+ var accountPassword = "12345"
+    accountCity = "Bengal"
+   let accountState;
+
+    // accountId = 2 not allowed 
+    accountEmail = "hgcj@google.com"
+    accountPassword = "4782"
+// dont use the var in the javascript because of the scope isse []//
+
+    console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+ "use strict" //treat all code as newer version of javascript
+  
+//  alert(3+3) we are using node js not any browser
+console.log(3+3);
+
+console.log("gautam")
+/* 1 tc39es ecma documentation of js
+2 mdn documnet ation of js*/
+
+   let name = "Gautam" /*string*/
+   let age = 21
+   let isLoggedIn = false
+   /* number= 2 to 53
+      bigint
+      string = ""
+      boolean = true/false
+      null = standalone value 
+      undefined => jaise ki let state;
+      symbol => unique
+      object 
+      */
+     console.log(typeof undefined);//undefined type
+     console.log(typeof null); //Object
