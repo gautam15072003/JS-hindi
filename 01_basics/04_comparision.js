1f2e7a8eb450f304fe0216eecf1f386c86138d90
gautam15072003 <gautamsaini1507@gmail.com>
2025-01-18 18:53:10 +0000

    comparisions confusing

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+// console.log("1"<2);
+// console.log("2">1);
+
+
+console.log(null>0);/*here the comparision >,< ,>= and eauality check == works different the comparision convert the null to number treat it as zero
+*/console.log(null==0);
+console.log(null>=0);
+
+console.log("2"===2); //strict check === string not converted to number
