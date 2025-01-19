d26fa61f67054b3189762160ce86358c6d5737c7
gautam15072003 <gautamsaini1507@gmail.com>
2025-01-19 15:27:48 +0000

    string and its methods

---
+++ 01_basics/05_string.js
@@ -0,0 +1,25 @@
+ const name = "gautam"
+ const repositoraryCount = "1"
+ console.log(`my name is ${name}&my repositorary count is ${repositoraryCount} `)
+
+ const gameName = new String('Gautam-hc-com-kashish')
+ console.log(gameName[0]);
+
+ console.log(gameName.length)
+ console.log(gameName.charAt(2))
+ console.log(gameName.indexOf('d'))
+
+ const newStringOne = gameName.substring(0,4);
+ console.log(newStringOne)
+ const newStringTwo = gameName.slice(-8,4)
+ console.log(newStringTwo)
+
+ const aCommonName = "   Gautam   "
+ console.log(aCommonName)
+ console.log(aCommonName.trim())
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+// trim documentation 
+const url = "https://gautam.com/hitesh%20choudary"
+console.log(url.replace('%','='))
+console.log(url.includes('kashish'))
+console.log(gameName.split('-'))
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -11,12 +11,33 @@ const bigNumber = 456789n
 // refrence(non-primitive)
 // Array,objects,functions
 
-const heroes = ["shaktiman","doga"] ;//array
-let my objects={
- name:"gautam",
-    age:22,
-}
+// const heroes = ["shaktiman","doga"] ;//array
+// let my objects= {
+//  name:"gautam",
+//     age:22,
+// }
 
-const my function = function(){
-    console.log("hello world");
-}
+// const my function = function(){
+//     console.log("hello world")
+// }
+
+
+
+// ----------------memory types-------------------------------------
+
+// stack(primitve),heap(non-primitive)
+
+let mygirlFrienName = "kritika"
+let anotherGfName = mygirlFrienName
+ anotherGfName = "kashish"
+ console.log(mygirlFrienName)
+ console.log(anotherGfName)
+
+ let userOne = {
+    email: "gautamsaini1507@gamil.com",
+    upi: "gaut1507@oksbi"
+ }
+ let userTwo = userOne
+ userTwo.email = "kashish_b@gmail.com"
+ console.log(userTwo.email);
+ console.log(userOne.email);
