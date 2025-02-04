0a6ad2f6ea7f53cc1de50a09829a0b1c3920bd35
gautam15072003 <gautamsaini1507@gmail.com>
2025-02-04 07:41:35 +0000

    global and local scopes

--- 03_basics/01_funtions.js
+++ 03_basics/01_funtions.js
@@ -28,6 +28,39 @@ const result = addTwoNumber(3,4)
 // console.log("result:",result);
 
 function UserloggedIn(username){
+    if(username === undefined){
+        console.log("please enterusername")
+    }
     return`${username} just logged in`
 }
-console.log(UserloggedIn("hitesh"));
+console.log(UserloggedIn());
+
+
+function PriceofCart(val1,val2,...num1){ //rest of ...
+    return num1
+}
+console.log(PriceofCart(200,400,500,600,700))
+
+const user = {
+    username: "gautam",
+    price: 500
+}
+
+function handleObject (anyobject){
+    console.log(username is ${anyobject.username}and price of item is ${anyobject.price})
+}
+// handleObject(user)
+
+handleObject({
+    username: "kaalu",
+    price: 200
+})
+
+
+const myNewArray = [200,400,500,700,800]
+
+function returnSecondValue(getarray){
+        return getarray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([300,600,500,600]));
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,40 @@
+// var c = 300
+let a = 500
+if(true){
+    let a = 10
+    const b= 20
+   console.log("inner val:",a);
+    
+}
+//  console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+function one(){
+    const username = "Gautam";
+// console.log(username)
+    function two(){
+        const website = "youtube";
+        console.log(username);
+        
+    }
+    // console.log(website);
+    //  two()
+    
+}
+
+// one ()
+
+
+if (true) {
+    const username = "Gautam"
+    if (username === "Gautam") {
+        const website = "dhapli.com"
+        console.log(username+website)
+    }
+    console.log(website);
+    
+}
+console.log(username);
