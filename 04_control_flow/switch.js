982d0bccf84c9f61a385111aaf11a676b7879e4d
gautam15072003 <gautamsaini1507@gmail.com>
2025-02-04 19:27:26 +0000

    switch changes

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log(`month is january`);
+        break;
+    case 2:
+        console.log(`month is feburary`);
+        break;
+    case 3:
+        console.log(`month is march`);
+        break;
+    case 4:
+        console.log(`month is april`);
+        break;
+
+    default:   console.log(`default case`);
+    
+        break;
+}
